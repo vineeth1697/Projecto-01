06490d2f1d356dd0f8aa80d001b92da4589e2719
vineeth1697 <52394120+vineeth1697@users.noreply.github.com>
2019-07-28 10:13:18 +0530

    coding completed
    initial coding with reading and printing of one json file has been done

---
+++ FinalProject/Assets/Scripts/DataController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class DataController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+        string pathToFIle = Application.dataPath + "/InventoryFile/Food.json";
+        string text = File.ReadAllText(pathToFIle);
+
+         Inventory food = JsonUtility.FromJson<Inventory>(text);
+
+        //InventoryTest food = JsonConvert.DeserializeObject<InventoryTest>(text);
+
+        Debug.Log(text);
+
+        Debug.Log(food.Name);
+
+        foreach(InventoryTypes type in food.InventoryTypes)
+        {
+            Debug.Log(type.Name + "===" + type.Durability + "==" + type.ResaleValue);
+        }
+
+
+
+
+   
+    }
+
+    public class InventoryTest
+    {
+        public string Name { get; set; }
+        List<InventoryTypes> InventoryTypes { get; set; }
+        public string Image { get; set; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FinalProject/Assets/Scripts/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Inventory
+{
+    public string Name;
+   public List<InventoryTypes> InventoryTypes;
+    public string Image;
+}
