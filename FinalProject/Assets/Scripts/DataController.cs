8d1fbfee5b3603c0c81d70862726271d132a8963
vineeth1697 <52394120+vineeth1697@users.noreply.github.com>
2019-07-28 19:57:48 +0530

    end changes

--- FinalProject/Assets/Scripts/DataController.cs
+++ FinalProject/Assets/Scripts/DataController.cs
@@ -3,35 +3,64 @@ using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class DataController : MonoBehaviour
 {
+    public Text returntext;
     public Text CopyText;
     // Start is called before the first frame update
     private string[] files;
     private string display;
     string FilePath;
+    
+
     void Start()
     {
-
+        returntext.text = ("Press 9 to get back to the main menu");
+        //weapon.InventoryTypes.count
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            PlayerPrefs.SetInt("Exit", 9);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("Menu");
 
-        CopyText.text = getInvetoryText();
+
+        }
+        if (PlayerPrefs.GetInt("Menu") == 1)
+        {
+            CopyText.text = getInvetoryText("Weapons.json");
+        }
+        else if (PlayerPrefs.GetInt("Menu") == 4)
+        {
+            CopyText.text = getInvetoryText("Clothing.json");
+        }
+        else if (PlayerPrefs.GetInt("Menu")==2)
+        {
+            CopyText.text = getInvetoryText("Utensils.json");
+        }
+        else if (PlayerPrefs.GetInt("Menu")==3)
+        {
+            CopyText.text = getInvetoryText("Food.json");
+        }
 
     }
 
-    public string getInvetoryText()
+
+    public string getInvetoryText(string fileName)
     {
-        FilePath = Application.dataPath + "/InventoryFile/Weapons.json";
+       
+        FilePath = Application.dataPath + "/InventoryFile/"+fileName;
         string WeaponText = File.ReadAllText(FilePath);
         Inventory weapon = JsonUtility.FromJson<Inventory>(WeaponText);
         // CopyText.text = weapon.Name;
-
         foreach (InventoryTypes types in weapon.InventoryTypes)
         {
             string InventoryName = types.Name;
@@ -41,7 +70,7 @@ public class DataController : MonoBehaviour
             display = display + "Name:" + types.Name + "\nDurability:" + types.Durability + "\nResaleValue:" + types.ResaleValue + "\n\n\n";
 
         }
-
+        PlayerPrefs.DeleteAll();
         return display;
     }
 }
---
+++ FinalProject/Assets/Scripts/MenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuController:MonoBehaviour 
+{
+    public Text MenuText;
+    public static DataController dataController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuText.text = "1.Weapons\n2.Utilities\n3.Food \n4.Clothing";       
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            print("Calling Key 1");
+            PlayerPrefs.SetInt("Menu", 1);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("mainwindow");
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            print("Calling Key 2");
+            PlayerPrefs.SetInt("Menu", 4);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("mainwindow");
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlayerPrefs.SetInt("Menu", 2);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("mainwindow");
+        
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+            PlayerPrefs.SetInt("Menu", 3);
+            PlayerPrefs.Save();
+            SceneManager.LoadScene("mainwindow");
+
+        }
+
+    }
+
+}
