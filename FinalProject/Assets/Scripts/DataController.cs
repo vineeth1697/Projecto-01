eb217e2fbe46bd6c46e4e551bd01865774234840
vineeth1697 <52394120+vineeth1697@users.noreply.github.com>
2019-07-28 13:43:54 +0530

    initial read from json file and displaying in text area

--- FinalProject/Assets/Scripts/DataController.cs
+++ FinalProject/Assets/Scripts/DataController.cs
@@ -10,30 +10,38 @@ public class DataController : MonoBehaviour
     public Text CopyText;
     // Start is called before the first frame update
     private string[] files;
+    private string display;
+    string FilePath;
     void Start()
     {
-        string FilePath = Application.dataPath + "/InventoryFile/Weapons.json";
 
-        string WeaponText=File.ReadAllText(FilePath);
-        Inventory weapon = JsonUtility.FromJson<Inventory>(WeaponText);
-       // CopyText.text = weapon.Name;
-
-        foreach(InventoryTypes types in weapon.InventoryTypes)
-        {
-           string InventoryName= types.Name;
-            int InventaryDurability = types.Durability;
-            int InventoryResaleValue = types.ResaleValue;
-
-            CopyText.text = "Name:" + types.Name + "\n Durability:" + types.Durability + "\n ResaleValue:" + types.ResaleValue;
-                
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
 
+        CopyText.text = getInvetoryText();
+
+    }
+
+    public string getInvetoryText()
+    {
+        FilePath = Application.dataPath + "/InventoryFile/Weapons.json";
+        string WeaponText = File.ReadAllText(FilePath);
+        Inventory weapon = JsonUtility.FromJson<Inventory>(WeaponText);
+        // CopyText.text = weapon.Name;
+
+        foreach (InventoryTypes types in weapon.InventoryTypes)
+        {
+            string InventoryName = types.Name;
+            int InventaryDurability = types.Durability;
+            int InventoryResaleValue = types.ResaleValue;
+
+            display = display + "Name:" + types.Name + "\nDurability:" + types.Durability + "\nResaleValue:" + types.ResaleValue + "\n\n\n";
+
+        }
+
+        return display;
     }
 }
